d8ba06df705966ff489226d20544ee85d1ddf310
miyazawa-38 <223658116+miyazawa-38@users.noreply.github.com>
2026-09-04 19:00:31 +0900

    fix: change -sf to -sfn (install.sh)

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 DOTFILES="$HOME/dotfiles"
 
 # simbolic link
-ln -sf "$DOTFILES/.zshrc" "$HOME/.zshrc"
-ln -sf "$DOTFILES/.gitconfig" "$HOME/.gitconfig"
-ln -sf "$DOTFILES/nvim" "$HOME/.config/nvim"
-ln -sf "$DOTFILES/ghostty" "$HOME/.config/ghostty"
-ln -sf "$DOTFILES/starship.toml" "$HOME/.config/starship.toml"
+ln -sfn "$DOTFILES/.zshrc" "$HOME/.zshrc"
+ln -sfn "$DOTFILES/.gitconfig" "$HOME/.gitconfig"
+ln -sfn "$DOTFILES/nvim" "$HOME/.config/nvim"
+ln -sfn "$DOTFILES/ghostty" "$HOME/.config/ghostty"
+ln -sfn "$DOTFILES/starship.toml" "$HOME/.config/starship.toml"
