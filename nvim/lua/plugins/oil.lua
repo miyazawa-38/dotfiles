7b86f1d4e04361c49b00acf05b334baaabd73140
miyazawa-38 <223658116+miyazawa-38@users.noreply.github.com>
2026-09-17 21:16:05 +0900

    feat: file treeをoilにした

---
+++ nvim/lua/plugins/oil.lua
@@ -0,0 +1,33 @@
+return {
+  -- snacks explorer の <leader>e / <leader>E を外す
+  {
+    "folke/snacks.nvim",
+    keys = {
+      { "<leader>e", false },
+      { "<leader>E", false },
+    },
+  },
+
+  {
+    "stevearc/oil.nvim",
+    lazy = false,
+    dependencies = { "nvim-mini/mini.icons" },
+    keys = {
+      { "<leader>e", "<cmd>Oil<cr>", desc = "Oil (current file dir)" },
+      {
+        "<leader>E",
+        function()
+          require("oil").open(vim.fn.getcwd())
+        end,
+        desc = "Oil (cwd)",
+      },
+      { "-", "<cmd>Oil<cr>", desc = "Open parent directory (oil)" },
+    },
+    opts = {
+      default_file_explorer = true,
+      view_options = { show_hidden = true },
+      delete_to_trash = true,
+      skip_confirm_for_simple_edits = true,
+    },
+  },
+}
